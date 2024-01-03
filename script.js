1532c98a75c1c45d4b238b3f866550a5135444a8
christianc1599 <134043407+christianc1599@users.noreply.github.com>
2024-01-03 13:10:20 -0500

    forEach loops active classes
    Added for each loops to go through each panel. I also added an event listener to respond to the user clicking on an image and changing their class to active so that they adopt the flex of 5. I also added a removeActiveClasses function so that when a new picture is clicked, the previous one is no longer active so that the page highlights one card at a time

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel) => {
-    console.log(panel)
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
 }
